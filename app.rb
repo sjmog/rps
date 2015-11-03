aa5b429bb7b152d31dcd3616d14a540fcab4a9e2
jafrog <howeveririna@gmail.com>
2015-11-03 11:08:38 +0000

    Fix syntax error and convert session param to symbol
    Also move presentation logic from view to controller helper

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/turn'
+require './lib/opponent'
 
 class Rps < Sinatra::Base
   enable :sessions
@@ -15,15 +16,30 @@ class Rps < Sinatra::Base
 
   get '/play' do
     @turn = Turn.new(session)
+    @endgame_message = endgame_message
     erb :play
   end
 
   post '/play' do
-    session[:player_shape] = params[:shape]
+    session[:player_shape] = params[:shape].downcase.to_sym
     session[:opponent_shape] = Opponent.new.shape
     redirect '/play'
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
+  private
+
+  def endgame_message
+    if @turn.opponent_shape
+      if @turn.draw?
+        "You draw!"
+      elsif @turn.win?
+        "You win!"
+      else
+        "You lose!"
+      end
+    end
+  end
 end
